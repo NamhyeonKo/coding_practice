12c584e077e9fb8ae2ece9ae3d335f819fc39105
NamhyunKo <glucosamin7@naver.com>
2022-04-20 06:03:25 +0900

    solve baekjoon-greedy problem -> delivery sugar

---
+++ baekjoon/greedy/deliversugar.c
@@ -0,0 +1,26 @@
+#pragma warning(disable: 4996)
+#include <stdio.h>
+
+int main(){
+    int N;
+    int cnt = 0, five, three = 0;
+
+    scanf("%d",&N);
+
+    five = N / 5 + 1;
+
+    while(1){
+        five--;
+        three = (N - five*5) / 3;
+
+        if(five*5+three*3==N){
+            cnt = five + three;
+            printf("%d",cnt);
+            break;
+        }
+        else if(five <= 0){
+            printf("-1");
+            return 0;
+        }
+    }    
+}
