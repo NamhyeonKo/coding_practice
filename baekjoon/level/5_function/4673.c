b83891ed01d5c501d200e000881ecadef61823c8
NamhyunKo <glucosamin7@naver.com>
2022-04-25 22:50:07 +0900

    solve boj-level -> function 4673

--- baekjoon/level/5_function/4673.c
+++ baekjoon/level/5_function/4673.c
@@ -4,8 +4,27 @@
 #include <string.h>
 #include <stdlib.h>
 
+int generator(int n){
+    int res = n;
+
+    while(n>0){
+        res += n % 10;
+        n /= 10;
+    }
+    return res;
+}
+
 int main(){
-    
+    int d[10000]={0};
+    int i, num;
+
+    for(i=1;i<=10000;i++){
+        num = generator(i);
+        if(num<=10000)d[num-1]=1;
+    }
+
+    for(i=1;i<=10000;i++)
+        if(d[i-1]==0)printf("%d\n",i);
 
     return 0;
 }
