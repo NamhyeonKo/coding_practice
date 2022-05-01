d62132832d20477eaf76613cea596510f3dd4cca
NamhyunKo <glucosamin7@naver.com>
2022-05-01 17:17:56 +0900

    solve boj-25083

---
+++ baekjoon/level/1_ioAndCalculations/25083.c
@@ -0,0 +1,16 @@
+// 새싹
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+    printf("         ,r\'\"7\n");
+    printf("r`-_   ,\'  ,/\n");
+    printf(" \\. \". L_r\'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |\n");
+
+    return 0;
+}
